8c3e0d067328a796662622a93b2add89ba5aa578
rdb <git@rdb.name>
2020-05-12 09:26:54 +0200

    Bump version to 1.8

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Import Panda3D .egg models",
     "author": "rdb",
-    "version": (1, 7),
+    "version": (1, 8),
     "blender": (2, 80, 0),
     "location": "File > Import > Panda3D (.egg)",
     "description": "",
